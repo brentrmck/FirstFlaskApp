19bd97d8880c7deb298d9a71c6c7a143eefa573d
brentmckinney <brentrmck@gmail.com>
2025-01-30 14:14:29 -0500

    trying again

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return "Hello from Treehouse"
 
+app.run(debug=True, port=8000, host='0.0.0.0')
 
-if __name__ == '__main__':
-    app.run()
-    print("Please work please")
+#if __name__ == '__main__':
+#    app.run()
