295b0ffe5787e67df4b39ed6fa34de708cbff4fc
brentmckinney <brentrmck@gmail.com>
2025-01-30 14:19:10 -0500

    trying again

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 @app.route('/')
-def index():
-    return "Hello from Treehouse"
+def index(name="Treehouse"):
+    return "Hello from {}".format(name)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
 
