ca4d7ec2c5ad73252a2acc5db552cca82fdcb60a
brentmckinney <brentrmck@gmail.com>
2025-02-06 18:16:21 -0500

    adding blocks

--- app.py
+++ app.py
@@ -5,15 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')
 def index(name="Treehouse"):
-    return "Hello from {}".format(name)
+    return render_template("index.html", name=name)
 
 @app.route('/add/<int:num1>/<int:num2>')
 def add(num1, num2):
     return render_template("add.html", num1=num1, num2=num2)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
-
-#if __name__ == '__main__':
-#    app.run()
-
-# Test commit / push
